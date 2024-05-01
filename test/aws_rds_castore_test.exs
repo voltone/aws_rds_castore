645a389aa5cefa8a8f39a9872b0f106b5fcedaee
Bram Verburg <bram.verburg@voltone.net>
2024-05-01 11:05:29 +0200

    Avoid :uri_string.parse/1 (#6)

--- test/aws_rds_castore_test.exs
+++ test/aws_rds_castore_test.exs
@@ -21,20 +21,47 @@ defmodule AwsRdsCAStoreTest do
       assert 'some.host.name' = ssl_opts[:server_name_indication]
     end
 
-    if :erlang.system_info(:otp_release) |> List.to_integer() >= 21 do
-      test "with url" do
-        ssl_opts = AwsRdsCAStore.ssl_opts("postgres://postgres:postgres@some.host.name/my_app_db")
-        assert :verify_peer = ssl_opts[:verify]
-        assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
-        assert 'some.host.name' = ssl_opts[:server_name_indication]
-      end
+    test "with url" do
+      ssl_opts = AwsRdsCAStore.ssl_opts("postgres://postgres:postgres@some.host.name/my_app_db")
+      assert :verify_peer = ssl_opts[:verify]
+      assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
+      assert 'some.host.name' = ssl_opts[:server_name_indication]
+    end
 
-      test "with url (charlist)" do
-        ssl_opts = AwsRdsCAStore.ssl_opts('postgres://postgres:postgres@some.host.name/my_app_db')
-        assert :verify_peer = ssl_opts[:verify]
-        assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
-        assert 'some.host.name' = ssl_opts[:server_name_indication]
-      end
+    test "with url (charlist)" do
+      ssl_opts = AwsRdsCAStore.ssl_opts('postgres://postgres:postgres@some.host.name/my_app_db')
+      assert :verify_peer = ssl_opts[:verify]
+      assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
+      assert 'some.host.name' = ssl_opts[:server_name_indication]
+    end
+
+    test "with url with special characters" do
+      ssl_opts =
+        AwsRdsCAStore.ssl_opts("postgres://postgres:secret[]123!{}@some.host.name/my_app_db")
+
+      assert :verify_peer = ssl_opts[:verify]
+      assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
+      assert 'some.host.name' = ssl_opts[:server_name_indication]
+    end
+  end
+
+  describe "Erlang API" do
+    test "file_path/0" do
+      assert String.ends_with?(:aws_rds_castore.file_path(), "/priv/global-bundle.pem")
+    end
+
+    test "ssl_opts/1 (charlist)" do
+      ssl_opts = :aws_rds_castore.ssl_opts('some.host.name')
+      assert :verify_peer = ssl_opts[:verify]
+      assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
+      assert 'some.host.name' = ssl_opts[:server_name_indication]
+    end
+
+    test "ssl_opts/1 (binary)" do
+      ssl_opts = :aws_rds_castore.ssl_opts("some.host.name")
+      assert :verify_peer = ssl_opts[:verify]
+      assert String.ends_with?(ssl_opts[:cacertfile], "/priv/global-bundle.pem")
+      assert 'some.host.name' = ssl_opts[:server_name_indication]
     end
   end
 end
