7ec6957fecd2fd866bb71d7020210ee95c81a60d
Milton Mazzarri <me@milmazz.uno>
2024-06-06 10:37:42 -0500

    add Mix Task to Hex package

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule AwsRdsCAStore.MixProject do
   use Mix.Project
 
-  @version "1.2.0"
+  @version "1.2.1-dev"
   @repo_url "https://github.com/voltone/aws_rds_castore"
 
   def project do
@@ -44,7 +44,7 @@ defmodule AwsRdsCAStore.MixProject do
   defp package do
     [
       files: [
-        "lib/aws_rds_castore.ex",
+        "lib",
         "priv",
         "src",
         "mix.exs",
